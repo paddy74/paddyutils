76aa5b4a407b493dbe5ff9b9d63ba225dca9453d
Patrick Cox <pxcox003@odu.edu>
2019-04-09 13:59:28 -0400

    Create additiveMerge for unordered_map

---
+++ include/paddyutils/functions/additiveMerge.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "base/stdUtilities.h"
+
+
+namespace paddyutils
+{
+
+/**
+ * @brief Merge b into a (inplace). The values of matching keys are summed.
+ *
+ * @tparam KEY_T
+ * @tparam VALUE_T
+ * @param a
+ * @param b
+ */
+template<typename KEY_T, typename VALUE_T>
+void additiveMergeInplace(
+    std::unordered_map<KEY_T, VALUE_T> & a,
+    std::unordered_map<KEY_T, VALUE_T> const & b
+)
+{
+    for (auto const & it = b.begin(); it != b.end(); ++it)
+        a[it->first] += it->second;
+}
+
+
+/**
+ * @brief Merge b into a new copy of a. The values of matching keys are summed.
+ *
+ * @tparam KEY_T
+ * @tparam VALUE_T
+ * @param a
+ * @param b
+ * @return std::unordered_map<KEY_T, VALUE_T>
+ */
+template<typename KEY_T, typename VALUE_T>
+std::unordered_map<KEY_T, VALUE_T> additiveMerge(
+    std::unordered_map<KEY_T, VALUE_T> a,
+    std::unordered_map<KEY_T, VALUE_T> const & b
+)
+{
+    additiveMergeInplace(a, b);
+
+    return a;
+}
+
+}
