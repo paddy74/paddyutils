e7261983aefb95886811c9e64208c920426bd7b3
Patrick Cox <pxcox003@odu.edu>
2019-04-17 10:05:02 -0400

    Remove broken split on string function

--- include/paddyutils/functions/strSplit.h
+++ include/paddyutils/functions/strSplit.h
@@ -36,34 +36,7 @@ std::vector<std::string> strSplit(std::string const & str, char const & delim)
 }
 
 
-/**
- * @brief Split a string on a string delimiter.
- *
- * @param str
- * @param delim
- * @return std::vector<std::string>
- */
-std::vector<std::string> strSplit(std::string const & str, std::string const & delim)
-{
-    std::vector<std::string> tokens;
-
-    // Skip delimiters at the beginning
-    std::string::size_type lastPos = str.find_first_not_of(delim, 0);
-    // Find first non-delimiter
-    std::string::size_type pos = str.find_first_of(delim, lastPos);
-
-    while ((std::string::npos != pos || std::string::npos != lastPos))
-    {
-        // Found token, add to the token vector
-        tokens.push_back(str.substr(lastPos, pos-lastPos));
-        // Skip delimiters
-        lastPos = str.find_first_not_of(delim, pos);
-        // Find next non-delimiter
-        pos = str.find_first_of(delim, lastPos);
-    }
-
-    return tokens;
-}
+// TODO: Split on string delim
 
 
 /**
@@ -73,26 +46,6 @@ std::vector<std::string> strSplit(std::string const & str, std::string const & d
  * @return std::vector<std::string>
  */
 std::vector<std::string> strSplit(std::string const & str)
-{
-    const char delim = ' ';
-    std::vector<std::string> tokens;
-
-    // Skip delimiters at the beginning
-    std::string::size_type lastPos = str.find_first_not_of(delim, 0);
-    // Find first non-delimiter
-    std::string::size_type pos = str.find_first_of(delim, lastPos);
-
-    while ((std::string::npos != pos || std::string::npos != lastPos))
-    {
-        // Found token, add to the token vector
-        tokens.push_back(str.substr(lastPos, pos-lastPos));
-        // Skip delimiters
-        lastPos = str.find_first_not_of(delim, pos);
-        // Find next non-delimiter
-        pos = str.find_first_of(delim, lastPos);
-    }
-
-    return tokens;
-}
+{ return strSplit(str, ' '); }
 
 }
