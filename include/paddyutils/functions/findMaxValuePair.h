be9c7b8e5ff493492d3a365ad8d4ae2bd9ea3ac0
Patrick Cox <coxpatrick74@gmail.com>
2019-05-02 19:44:40 -0400

    Segfault proof check for findMaxValuePair

--- include/paddyutils/functions/findMaxValuePair.h
+++ include/paddyutils/functions/findMaxValuePair.h
@@ -2,59 +2,29 @@
 
 #include "base/stdPaddyUtils.h"
 
+namespace paddyutils {
 
-namespace paddyutils
-{
-    /**
-     * @brief Find the maximum key-value pair in an `unorderd_map`.
-     *  Based on: https://stackoverflow.com/a/34937216/7706917
-     *
-     * @tparam KEY_T
-     * @tparam VALUE_T
-     * @param x
-     * @return std::pair<KEY_T, VALUE_T>
-     */
-    template<typename KEY_T, typename VALUE_T>
-    std::pair<KEY_T, VALUE_T> findMaxValuePair(
-        std::unordered_map<KEY_T, VALUE_T> const & x
-    )
-    {
-        using pairtype=std::pair<KeyType,ValueType>;
+/**
+ * @brief Find the maximum key-value pair in std::pair value_type container.
+ *
+ * @tparam KEY_T
+ * @tparam VALUE_T
+ * @param x
+ * @return std::pair<KEY_T, VALUE_T>
+ */
+template <class Container>
+auto findMaxValuePair(Container const &x) -> typename Container::value_type {
+  using value_t = typename Container::value_type;
 
-        return *std::max_element(
-            x.begin(),
-            x.end(),
-            [] (const pairtype & p1, const pairtype & p2)
-            {
-                return p1.second < p2.second;
-            }
-        );
-    }
+  if (x.size() == 0)
+    return value_t();
+  if (x.size() == 1)
+    return *x.begin();
 
-
-    /**
-     * @brief Find the maximum key-value pair in a `map`.
-     *  Based on: https://stackoverflow.com/a/34937216/7706917
-     *
-     * @tparam KEY_T
-     * @tparam VALUE_T
-     * @param x
-     * @return std::pair<KEY_T, VALUE_T>
-     */
-    template<typename KEY_T, typename VALUE_T>
-    std::pair<KEY_T, VALUE_T> findMaxValuePair(
-        std::map<KEY_T, VALUE_T> const & x
-    )
-    {
-        using pairtype=std::pair<KeyType,ValueType>;
-
-        return *std::max_element(
-            x.begin(),
-            x.end(),
-            [] (const pairtype & p1, const pairtype & p2)
-            {
-                return p1.second < p2.second;
-            }
-        );
-    }
+  const auto compare = [](value_t const &p1, value_t const &p2) {
+    return p1.second < p2.second;
+  };
+  return *std::max_element(x.begin(), x.end(), compare);
 }
+
+} // namespace paddyutils
