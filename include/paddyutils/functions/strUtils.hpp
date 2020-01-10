e2be1cbee56fffbb225588b391d4efac2a287a1c
Patrick Cox <coxpatrick74@gmail.com>
2020-01-10 17:02:19 -0500

    Add whitespace trimming

--- include/paddyutils/functions/strUtils.hpp
+++ include/paddyutils/functions/strUtils.hpp
@@ -17,4 +17,41 @@ bool isNumber(std::string const & str)
         str, std::regex(("((\\+|-)?[[:digit:]]+)(\\.(([[:digit:]]+)?))?")));
 }
 
+/**
+ * @brief Trim whitespace from the start of a string.
+ *
+ * @param s The string to trim.
+ */
+void ltrim(std::string & s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+                return !std::isspace(ch);
+            }));
+}
+
+/**
+ * @brief Trim whitespace from the end of a string.
+ *
+ * @param s The string to trim.
+ */
+void rtrim(std::string & s)
+{
+    s.erase(
+        std::find_if(
+            s.rbegin(), s.rend(), [](int ch) { return !std::isspace(ch); })
+            .base(),
+        s.end());
+}
+
+/**
+ * @brief Trim whitespace from both ends of a string.
+ *
+ * @param s The string to trim.
+ */
+void trim(std::string & s)
+{
+    ltrim(s);
+    rtrim(s);
+}
+
 }  // namespace paddyutils
